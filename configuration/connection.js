620114322566fd6b14afcc23479ddc6fd24a0101
carlos ordonez <ordonezdose@gmail.com>
2020-11-19 23:23:02 -0500

    heroku

--- configuration/connection.js
+++ configuration/connection.js
@@ -1,5 +1,16 @@
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL) {
+
+} else {
+  connection= mysql.createConnection({
+    host: 'fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'soynzeld1oc4y865',
+    password: 'p3v3gb3sf9c97xp1',
+    database: 'lk6aatjdrxxq6m2v',
+  });
+}
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
